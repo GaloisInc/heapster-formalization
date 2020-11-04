a466035ce443cb9be2d0c14a6168f5ec6b419f2b
Eddy Westbrook <westbrook@galois.com>
2020-11-04 10:59:12 -0800

    added more stuff to PermType.v; added PermType.v to the _CoqProject

--- src/PermType.v
+++ src/PermType.v
@@ -40,9 +40,23 @@ Record PermType (A B:Type) : Type :=
 Definition VPermType A := PermType Value A.
 Notation "xi : T @ xs" := (ptApp _ _ T xi xs) (at level 35).
 
-Definition star {Ai As Bs} (T:PermType Ai As) (U:PermType Ai Bs)
+Definition starPT {Ai As Bs} (T:PermType Ai As) (U:PermType Ai Bs)
   : PermType Ai (As * Bs) :=
   {| ptApp := fun ai abs => ai : T @ fst abs * ai : U @ snd abs |}.
+
+
+Definition exPerms {Ai As} {Bs:As -> Type}
+  (F : forall a, PermType Ai (Bs a)) : PermType Ai (sigT Bs) :=
+  {| ptApp := fun ai abs => ai : F (projT1 abs) @ (projT2 abs) |}.
+Notation "'ex' ( x : A ) . T" := (exPerms (As:=A) (fun x => T)) (at level 70).
+
+Definition either {A B C} (f:A -> C) (g:B -> C) (x:A+B): C :=
+  match x with inl a => f a | inr b => g b end.
+Definition or {Ai As Bs} (T1:PermType Ai As)
+  (T2:PermType Ai Bs) : PermType Ai (As + Bs) :=
+  {| ptApp := fun ai abs =>
+     either (ptApp _ _ T1 ai) (ptApp _ _ T2 ai) abs |}.
+
 
 Variant RW: Type := R | W.
 
@@ -81,3 +95,15 @@ Definition timesPT {A1 A2 B1 B2}
            (T1:PermType A1 B1) (T2:PermType A2 B2) : PermType (A1*A2) (B1*B2) :=
   {| ptApp := fun a12 b12 =>  fst a12 : T1 @ fst b12 * snd a12 : T2 @ snd b12 |}.
 Notation "T1 *T* T2" := (plusPT T1 T2) (at level 40).
+
+Program Definition equals_perm {A} (a1 a2 : A): @perm (Si*Ss) := {|
+  rely _ _ := True; guar s1 s2 := s1=s2; pre _ := a1=a2; |}.
+Next Obligation.
+repeat constructor.
+Defined.
+Next Obligation.
+repeat constructor. intros x y z e1 e2. transitivity y; assumption.
+Defined.
+
+Definition eqp {A} (a:A): PermType A unit :=
+  {| ptApp := fun a' _ => singleton_Perms (equals_perm a a') |}.
